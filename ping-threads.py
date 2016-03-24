08cf2b8feb78b0a38a92d6835a3dd1c76c683ace
Jude Quintana <jude.quintana@gmail.com>
2016-03-24 00:00:11 -0600

    added verbosity

--- ping-threads.py
+++ ping-threads.py
@@ -5,32 +5,35 @@ from queue import Queue
 from ipaddress import IPv4Interface
 
 
-def pinger(queue):
+def pinger(queue, args):
     """Pings subnet"""
     while True:
         ip = queue.get()
-        # print("Thread %s: Pinging %s" % (i, ip))
-        # print
 
         ret = subprocess.call(["ping", "-c", "1", ip],
                               stdout=subprocess.DEVNULL,
                               stderr=subprocess.STDOUT)
-        if ret == 0:
-            print("%s: is alive" % ip)
-            print
-        # else:
-        #     print("%s: did not respond" % ip)
-        #     print
+        print_ip_mesgs(ret, ip, args)
         queue.task_done()
 
 
 def cli_parse():
     parser = argparse.ArgumentParser(description='Will ping all address in parallel')
     parser.add_argument('subnet', help='Input network subnet ie(192.168.0.1/24)')
+    parser.add_argument('-v', '--verbose', help='display what hosts are not responding', action='store_true')
 
     args = parser.parse_args()
 
     return vars(args)
+
+
+def print_ip_mesgs(success, ip, args):
+    if success == 0:
+        print("%s: is alive" % ip)
+        print
+    elif args.get('verbose'):
+        print("%s: did not respond" % ip)
+        print
 
 
 def check_subnet(subnet):
@@ -45,9 +48,9 @@ def get_hosts_from_subnet(subnet):
     return [ip for ip in subnet.network.hosts()]
 
 
-def spawn_thread_pool(num_threads, queue):
+def spawn_thread_pool(num_threads, queue, args):
     for num in range(num_threads):
-        worker = Thread(target=pinger, args=(queue,), daemon=True)
+        worker = Thread(target=pinger, args=(queue, args), daemon=True)
         worker.start()
 
 
@@ -71,7 +74,7 @@ def main():
     ips = get_hosts_from_subnet(subnet)
     queue = create_queue()
 
-    spawn_thread_pool(len(ips), queue)
+    spawn_thread_pool(len(ips), queue, args)
     place_work_in_queue(ips, queue)
     wait_for_worker_threads(queue)
 
